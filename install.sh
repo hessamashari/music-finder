a28482fb38909d5ca020458c736f5a7d3b986e68
Hessam Ashari <hessam.ashari@gmail.com>
2018-07-22 19:20:19 +0430

    delete uninstall.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,6 @@ fi
 if [[ "$check_distro" == "0" ]]; then
     sudo cp music-finder /usr/bin/music-finder 
     sudo chmod 755 /usr/bin/music-finder
-    sudo mv -r ../Music_Finder $HOME/.config/
 # bug : if user doesnt enter password script print music-finder succesfully installed
     echo -e "\n ${GRE}music-finder succesfully installed "
 else

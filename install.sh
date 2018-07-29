875e67a7328eba4c3ec95f659ff5dbaf9d2ac98b
Hessam Ashari <hessam.ashari@gmail.com>
2018-07-29 18:40:34 +0430

    fix bugs

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 clear
 
-noColor='\033[0m'       # Text mode
+normal="$(printf '\033[0m')"          # Text mode
 # Colors
-red='\033[0;31m'          # red
-green='\033[0;32m'        # green
-orange='\033[0;33m'       # orange
-blue='\033[0;34m'         # blue
-white='\033[0;37m'        # white
+red="$(printf '\033[0;31m')"  		  # red
+green="$(printf '\033[0;32m')"        # green
+orange="$(printf '\033[0;33m')"       # orange
+blue="$(printf '\033[0;34m')"         # blue
+white="$(printf '\033[0;37m')"        # white
 
 # ----------\Main\---------
 check_distro="0"
 #Checking if the distro is DebianBase / ArchBase / RedHatBase / SuseBase and running the correct command
 
-echo -e "${green} Enter your password for install dependencies\n${noColor}"
+echo -e "${green} Enter your password for install dependencies\n${normal}"
 if pacman -Q &> /dev/null; then # Check Arch
 	sudo pacman -S mplayer
 	# Check user's entered password
@@ -40,7 +40,7 @@ elif zypper search i+ &> /dev/null; then # Check openSUSE
 		check_distro="1"
 	fi
 else
-	echo -e "${red}Your distro is neither ArchBase or DebianBase or RedHatBase or SuseBase So, The script is not going to work in your distro and you have install it manually. for more information read README.md${noColor}"
+	echo -e "${red}Your distro is neither ArchBase or DebianBase or RedHatBase or SuseBase So, The script is not going to work in your distro and you have install it manually. for more information read README.md${normal}"
 	check_distro="1"
 fi
 
@@ -48,8 +48,8 @@ fi
 if [ "$check_distro" == "0" ]; then
     sudo cp music-finder /usr/bin/music-finder
     sudo chmod 755 /usr/bin/music-finder
-    echo -e "\n ${green}music-finder succesfully installed ${noColor}"
+    echo -e "\n ${green}music-finder succesfully installed ${normal}"
 else
-	echo -e "\n ${red}music-finder doesn't install succesfully ${noColor}"
+	echo -e "\n ${red}music-finder doesn't install succesfully ${normal}"
 	exit 1
 fi
